0930d06763733ee20f4c4d81939f2bb71e9216ca
pprecel <filip.strozik@outlook.com>
2019-11-11 10:30:28 +0100

    Add envconfig reader; Add logs to main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"github.com/pPrecel/BeerKongServer/internal/auth"
 	"github.com/pPrecel/BeerKongServer/internal/handlers"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"github.com/vrischmann/envconfig"
 )
 
+type Config struct {
+	Port string `envconfig:"PORT"`
+}
 
 func main() {
+	log.Infof("Start server with envs")
+
+	log.Infof("Read envconfigs")
+	var conf Config
+	err := envconfig.Init(&conf)
+	if err != nil {
+		log.Fatalf("Env error: %s", err.Error())
+	}
+
+	log.Infof("Create auth communicator")
 	authClient := auth.New(&http.Client{})
 
+	log.Infof("Create handlers")
 	handlers := handlers.New(authClient)
+
+	log.Infof("Set handlers")
 	http.HandleFunc("/", handlers.GraphQlHandler)
-	http.ListenAndServe(":80", nil)
-	log.Info("The server starts listening on port 80")
+
+	log.Infof("Start listening on port \":%s\"", conf.Port)
+	http.ListenAndServe(fmt.Sprintf(":%s", conf.Port), nil)
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.13
 
 require (
 	github.com/sirupsen/logrus v1.4.2
-	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
+	github.com/vrischmann/envconfig v1.2.0
 )
